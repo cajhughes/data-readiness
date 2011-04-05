b4673e520c638140ffe8e4b7e7f6b9c131bb4adc
cajhughes <cajhughes@78065b15-ba9d-9902-9579-a95e9a2c38bc>
2011-04-05 22:12:42 +0000

    Initial commit of preliminary code

---
+++ Project/src/com/cajhughes/ready/Result.java
@@ -0,0 +1,141 @@
+package com.cajhughes.ready;
+
+import java.math.BigDecimal;
+
+public class Result {
+    private int positiveQuantityPositivePrice = 0;
+    private int positiveQuantityZeroPrice = 0;
+    private int positiveQuantityNegativePrice = 0;
+
+    private int zeroQuantityPositivePrice = 0;
+    private int zeroQuantityZeroPrice = 0;
+    private int zeroQuantityNegativePrice = 0;
+
+    private int negativeQuantityPositivePrice = 0;
+    private int negativeQuantityZeroPrice = 0;
+    private int negativeQuantityNegativePrice = 0;
+
+    private int quantityNotNumber = 0;
+    private int priceNotNumber = 0;
+
+    public int getNegQtyPosPrice() {
+        return negativeQuantityPositivePrice;
+    }
+
+    public int getNegQtyZeroPrice() {
+        return negativeQuantityZeroPrice;
+    }
+
+    public int getNegQtyNegPrice() {
+        return negativeQuantityNegativePrice;
+    }
+
+    public int getPosQtyPosPrice() {
+        return positiveQuantityPositivePrice;
+    }
+
+    public int getPosQtyZeroPrice() {
+        return positiveQuantityZeroPrice;
+    }
+
+    public int getPosQtyNegPrice() {
+        return positiveQuantityNegativePrice;
+    }
+
+    public int getPriceNotNumber() {
+        return priceNotNumber;
+    }
+
+    public int getQtyNotNumber() {
+        return quantityNotNumber;
+    }
+
+    public int getZeroQtyPosPrice() {
+        return zeroQuantityPositivePrice;
+    }
+
+    public int getZeroQtyZeroPrice() {
+        return zeroQuantityZeroPrice;
+    }
+
+    public int getZeroQtyNegPrice() {
+        return zeroQuantityNegativePrice;
+    }
+
+    public void process(final String quantity, final String price) {
+        int qty;
+        BigDecimal money;
+        try {
+            qty = getInt(quantity);
+            try {
+                money = getBigDecimal(price);
+                setResult(qty, money);
+            }
+            catch(NumberFormatException nfe) {
+                priceNotNumber++;
+            }
+        }
+        catch(NumberFormatException nfe) {
+            quantityNotNumber++;
+        }
+    }
+
+    private void setResult(final int quantity, final BigDecimal price) {
+        int priceCompare = price.compareTo(BigDecimal.ZERO);
+        if(quantity > 0) {
+            if(priceCompare == 1) {
+                positiveQuantityPositivePrice++;                
+            }
+            else if(priceCompare == 0) {
+                positiveQuantityZeroPrice++;
+            }
+            else {
+                positiveQuantityNegativePrice++;
+            }
+        }
+        else if(quantity == 0) {            
+            if(priceCompare == 1) {
+                zeroQuantityPositivePrice++;                
+            }
+            else if(priceCompare == 0) {
+                zeroQuantityZeroPrice++;
+            }
+            else {
+                zeroQuantityNegativePrice++;
+            }
+        }
+        else {
+            if(priceCompare == 1) {
+                negativeQuantityPositivePrice++;                
+            }
+            else if(priceCompare == 0) {
+                negativeQuantityZeroPrice++;
+            }
+            else {
+                negativeQuantityNegativePrice++;
+            }
+        }
+    }
+
+    private int getInt(final String quantity) throws NumberFormatException {
+        int value;
+        if(quantity != null) {
+            value = Integer.parseInt(quantity);
+        }
+        else {
+            throw new NumberFormatException();
+        }
+        return value;
+    }
+
+    private BigDecimal getBigDecimal(final String price) throws NumberFormatException {
+        BigDecimal value;
+        if(price != null) {
+            value = new BigDecimal(price);
+        }
+        else {
+            throw new NumberFormatException();
+        }
+        return value;
+    }
+}
